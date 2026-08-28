2e73d6ce717b86c9c8e5acfb568d58788895861f
Reihan Demasiras Challada <179684859+reihandems@users.noreply.github.com>
2026-08-28 16:38:39 +0700

    feature: Search blog features

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,25 +1,27 @@
-function Search() {
+function Search({onSearchChange, inputSearch, totalPosts, onKeySearch, search}) {
     return (
         <>
-            <label className="input mb-2">
-                <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <g
-                    strokeLinejoin="round"
-                    strokeLinecap="round"
-                    strokeWidth="2.5"
-                    fill="none"
-                    stroke="currentColor"
-                    >
-                    <circle cx="11" cy="11" r="8"></circle>
-                    <path d="m21 21-4.3-4.3"></path>
-                    </g>
-                </svg>
-                <input type="search" className="grow" placeholder="Search" />
-                <kbd className="kbd kbd-sm">⌘</kbd>
-                <kbd className="kbd kbd-sm">K</kbd>
-            </label>
+            <div className="flex flex-row gap-2">
+                <label className="input mb-2">
+                    <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <g
+                        strokeLinejoin="round"
+                        strokeLinecap="round"
+                        strokeWidth="2.5"
+                        fill="none"
+                        stroke="currentColor"
+                        >
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <path d="m21 21-4.3-4.3"></path>
+                        </g>
+                    </svg>
+                    <input onChange={(event) => inputSearch(event.target.value)} value={search} onKeyDown={onKeySearch} type="text" className="grow" placeholder="Search Article..." />
+                    <kbd className="kbd kbd-sm">Enter</kbd>
+                </label>
+                <button onClick={onSearchChange} className="btn btn-neutral">Cari</button>
+            </div>
         
-            <p className="text-sm text-gray-600 mb-3">Ditemukan 0 data dengan kata kunci</p>
+            <p className="text-sm text-gray-600 mb-3">Ditemukan {totalPosts} data dengan kata kunci {search === "" ? "[keyword]" : search}</p>
         </>
     )
 }
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,18 +1,46 @@
+import { useState } from "react"
+
 import Search from "../components/Search"
 import Article from "../components/Article"
 import PostData from "../posts.json"
 
 function Home() {
+    const [posts, setPosts] = useState(PostData);
+    const [search, setSearch] = useState("");
+    const [totalPosts, setTotalPosts] = useState(0);
+
+    const onSearchChange = () => {
+        const filteredPost = PostData.filter((item) => {
+            return item.title.toLowerCase().includes(search.toLowerCase());
+        });
+        setPosts(filteredPost);
+        setTotalPosts(filteredPost.length);
+    }
+
+    console.log(search);
+    
+    const onKeySearch = (event) => {
+        if (event.key === "Enter") {
+            onSearchChange()
+        }
+    }
+
     return (
         <>
-            <div className="font-mono flex flex-col justify-center items-center h-auto p-8">
+            <div className="font-mono flex flex-col justify-center items-center min-h-screen p-8">
                 <h1 className="text-3xl font-bold mb-1">Simple Blog</h1>
                 <p className="text-gray-500 mb-3">Find your favorite blog.</p>
                 
-                <Search />
+                <Search
+                    onSearchChange={onSearchChange}
+                    inputSearch={setSearch}
+                    totalPosts={totalPosts}
+                    onKeySearch={onKeySearch}
+                    search={search}
+                />
 
                 <div className="flex flex-wrap max-w-96 w-full gap-4">
-                    {PostData.map((props, index) => (
+                    {posts.map((props, index) => (
                         <Article key={index} {...props} />
                     ))}
                 </div>
